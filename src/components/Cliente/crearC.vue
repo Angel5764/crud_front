1ea218c1a13ad2986f9dc7805b513087f1c11a7a
Angel Zea <angeljavier57648628@gmail.com>
2023-03-18 19:23:32 -0600

    login y dashboard

---
+++ src/components/Cliente/crearC.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">Agregar Cliente</div>
+      <div class="card-body">
+        <form v-on:submit.prevent="agregarRegistro">
+          <div class="form-group">
+            <label for="">Nombre:</label>
+            <input
+              type="text"
+              class="form-control"
+              name="nombre"
+              v-model="Cliente.nombre"
+              aria-describedby="helpId"
+              id="nombre"
+              placeholder="Nombre"
+            />
+            <label for="">Apellidos:</label>
+            <input
+              type="text"
+              class="form-control"
+              name="apellidos"
+              v-model="Cliente.apellido"
+              aria-describedby="helpId"
+              id="apellido"
+              placeholder="Apellidos"
+            />
+            <small id="helpId" class="form-text" text-muted
+              >Ingresa un numero de telefono</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="">Telefono:</label>
+            <input
+              type="text"
+              class="form-control"
+              name="telefono"
+              id="telefono"
+              v-model="articulo.proveedor"
+              aria-describedby="helpId"
+              placeholder="Telefono"
+            />
+            <small id="helpId" class="form-text" text-muted
+              >Ingresa un email:</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="">Email:</label>
+            <input
+              type="text"
+              class="form-control"
+              name="email"
+              id="email"
+              v-model="articulo.email"
+              aria-describedby="helpId"
+              placeholder="Email"
+            />
+            <small id="helpId" class="form-text" text-muted
+              >Ingresa el precio del articulo</small
+            >
+          </div>
+
+          <br />
+
+          <div class="btn-group" role="group">
+            |<button type="submit" class="btn btn-success">Agregar</button>|
+            |<router-link :to="{ name: 'listar' }" class="btn btn-danger"
+              >Cancelar</router-link
+            >|
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      articulo: {},
+    };
+  },
+
+  methods: {
+    agregarRegistro() {
+      console.log(this.articulo);
+
+      var datosEnviar = {
+        name: this.articulo.name,
+        proveedor: this.articulo.proveedor,
+        precio: this.articulo.precio,
+      };
+
+      axios
+        .post("https://localhost:7053/articulos", datosEnviar)
+        .then((result) => {
+          console.log(result);
+          window.location.href = "Listar";
+        });
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,142 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import crearU from "../components/Usuario/crearU.vue";
+import editarU from "../components/Usuario/editarU.vue";
+import listarU from "../components/Usuario/listarU.vue";
+import crearC from "../components/Cliente/crearC.vue";
+import editarC from "../components/Cliente/editarC.vue";
+import listarC from "../components/Cliente/listarC.vue";
+import crearD from "../components/Departamento/crearD.vue";
+import editarD from "../components/Departamento/editarD.vue";
+import listarD from "../components/Departamento/listarD.vue";
+import crearE from "../components/Empleado/crearE.vue";
+import editarE from "../components/Empleado/editarE.vue";
+import listarE from "../components/Empleado/listarE.vue";
+import crearF from "../components/Factura/crearF.vue";
+import editarF from "../components/Factura/editarF.vue";
+import listarF from "../components/Factura/listarF.vue";
+import crearP from "../components/Puesto/crearP.vue";
+import editarP from "../components/Puesto/editarP.vue";
+import listarP from "../components/Puesto/listarP.vue";
+import crearR from "../components/Rol/crearR.vue";
+import editarR from "../components/Rol/editarR.vue";
+import listarR from "../components/Rol/listarR.vue";
+import Login from "../components/LoginZea.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "login",
+    component: Login,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
-    },
+    path: "/UsuarioCrear",
+    name: "crear",
+    component: crearU,
+  },
+  {
+    path: "/UsuarioEditar",
+    name: "listar",
+    component: listarU,
+  },
+  {
+    path: "/UsuarioListar",
+    name: "editar",
+    component: editarU,
+  },
+  {
+    path: "/ClienteCrear",
+    name: "crear",
+    component: crearC,
+  },
+  {
+    path: "/ClienteEditar",
+    name: "editar",
+    component: editarC,
+  },
+  {
+    path: "/ClienteListar",
+    name: "listar",
+    component: listarC,
+  },
+  {
+    path: "/DepartamentoCrear",
+    name: "crear",
+    component: crearD,
+  },
+  {
+    path: "/DepartamentoEditar",
+    name: "editar",
+    component: editarD,
+  },
+  {
+    path: "/DepartamentoListar",
+    name: "listar",
+    component: listarD,
+  },
+  {
+    path: "/EmpleadoCrear",
+    name: "crear",
+    component: crearE,
+  },
+  {
+    path: "/EmpleadoEditar",
+    name: "editar",
+    component: editarE,
+  },
+  {
+    path: "/EmpleadoListar",
+    name: "listar",
+    component: listarE,
+  },
+  {
+    path: "/FacturaCrear",
+    name: "crear",
+    component: crearF,
+  },
+  {
+    path: "/FacturaEditar",
+    name: "editar",
+    component: editarF,
+  },
+  {
+    path: "/FacturaListar",
+    name: "listar",
+    component: listarF,
+  },
+  {
+    path: "/PuestoCrear",
+    name: "crear",
+    component: crearP,
+  },
+  {
+    path: "/PuestoEditar",
+    name: "editar",
+    component: editarP,
+  },
+  {
+    path: "/PuestoListar",
+    name: "listar",
+    component: listarP,
+  },
+  {
+    path: "/RolCrear",
+    name: "crear",
+    component: crearR,
+  },
+  {
+    path: "/RolEditar",
+    name: "editar",
+    component: editarR,
+  },
+  {
+    path: "/RolListar",
+    name: "listar",
+    component: listarR,
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
